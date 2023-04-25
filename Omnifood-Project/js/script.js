b9491c01e90ae36242491561082ae9954c8a2951
Subhadeep <subhadeeppaul35@gmail.com>
2023-04-25 12:01:12 +0530

    📜 smooth scrolling function building

--- Omnifood-Project/js/script.js
+++ Omnifood-Project/js/script.js
@@ -7,3 +7,19 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function (){
     headerEl.classList.toggle("nav-open");
 });
+
+//Smooth scrolling animation
+
+const allLinks = document.querySelectorAll('a:link');
+// console.log(allLinks);
+
+allLinks.forEach(function(link){
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const href = link.getAttribute("href");
+
+
+        //scroll back top
+
+    });
+});
